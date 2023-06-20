0acb0a1244f1463fef19f1f2baca40daeb2072ff
kieran-calavan <93680051+kieran-calavan@users.noreply.github.com>
2023-06-20 10:26:09 -0500

    Update sanity-utils.ts

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,6 +1,6 @@
 import { createClient, groq } from "next-sanity";
-import { Project } from "@/types/Project";
 import clientConfig from './config/client-config'
+import { Project } from "@/types/Project";
 import { Page } from "@/types/Page";
 
 export async function getProjects(): Promise<Project[]> {
