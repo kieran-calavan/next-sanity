5aae249a5f4857cb31a281b6c2ce80e7f12f5aad
kieran-calavan <93680051+kieran-calavan@users.noreply.github.com>
2023-06-20 09:11:39 -0500

    updates

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,6 +1,6 @@
 import '../globals.css'
 import Link from "next/link"
-// import { getPages } from '@/sanity/sanity-utils';
+import { getPages } from '@/sanity/sanity-utils';
 
 export const metadata = {
   title: 'My Awesome Site',
@@ -12,7 +12,7 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  // const pages = await getPages();
+  const pages = await getPages();
 
   return (
     <html lang="en">
@@ -20,9 +20,9 @@ export default async function RootLayout({
         <header className="flex items-center justify-between">
           <Link href="/" className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-lg font-bold">Kieran</Link>
           <div className="flex items-center gap-5 text-sm text-gray-600">
-            {/* {pages.map((page) => (
+            {pages.map((page) => (
               <Link key={page._id} href={`/${page.slug}`} className="hover:underline">{page.title}</Link>
-            ))} */}
+            ))}
 
           </div>
 
--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,7 +1,7 @@
 import { createClient, groq } from "next-sanity";
-import { Project } from "@/types/project";
+import { Project } from "@/types/Project";
 import clientConfig from './config/client-config'
-import { Page } from "@/types/page";
+import { Page } from "@/types/Page";
 
 export async function getProjects(): Promise<Project[]> {
   return createClient(clientConfig).fetch(
@@ -32,9 +32,9 @@ export async function getProject(slug: string): Promise<Project> {
   )
 }
 
-export async function getPages (): Promise<Page[]> {
+export async function getPages(): Promise<Page[]> {
   return createClient(clientConfig).fetch(
-    groq`*[_type == "page]{
+    groq`*[_type == "page"]{
       _id,
       _createdAt,
       title,
@@ -43,15 +43,15 @@ export async function getPages (): Promise<Page[]> {
   )
 }
 
-export async function getPage (slug: string): Promise<Page> {
+export async function getPage(slug: string): Promise<Page> {
   return createClient(clientConfig).fetch(
-    groq`*[*_type == "page" && slug.current == $slug][0]{
+    groq`*[_type == "page" && slug.current == $slug][0]{
       _id,
       _createdAt,
       title,
       "slug": slug.current,
       content
     }`,
-    {slug}
+    { slug }
   )
 }
